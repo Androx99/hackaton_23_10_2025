88eb3d325e2e413cf908c55d07da867244b40774
sebasthevibecoder <lungugabrielsebastian@gmail.com>
2025-10-23 14:01:40 +0200

    EJ19

---
+++ Ejercicio 19 - Racha Poder/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Arrays;
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int racha = 0; //Inicializamos  variables de racha y aleatorio y arrays
+        Random aleatorio = new Random();
+        int [] numeros = new int[15];
+        //Ahora procedemos a rellenar el array de números aleatorios
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = aleatorio.nextInt(15);
+        }
+        System.out.println(Arrays.toString(numeros));
+        int maxRacha = 1;
+        int actual = 1;
+        for (int i = 0; i < numeros.length ; i++) {
+            if (numeros[i] == numeros [i + 1]){
+                actual++;}
+            else{
+                    actual = 1;}
+                if(actual > maxRacha); {
+                maxRacha = actual;
+            }
+
+        }
+        System.out.println("Racha máxima: " + maxRacha);
+
+
+    }
+}
